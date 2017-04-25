37c681895830850c13258a5b143a4c6e3f5ae6c6
Sebastian Uriel Flores <s.flores@outlook.com.ar>
2017-04-25 17:02:19 -0300

    Initializing

---
+++ FilesEncryptor/helpers/HammingEncoder.cs
@@ -0,0 +1,17 @@
+﻿using FilesEncryptor.dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesEncryptor.helpers
+{
+    public static class HammingEncoder
+    {
+        public static async Task Encode(EncodedString encodedStr, uint wordSize)
+        {
+
+        }
+    }
+}
