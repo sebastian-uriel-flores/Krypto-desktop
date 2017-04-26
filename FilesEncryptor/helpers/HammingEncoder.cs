6cfec9cc87735c6fa713387ff10a055f4a31d23d
Sebastian Uriel Flores <s.flores@outlook.com.ar>
2017-04-26 16:44:02 -0300

    Defining Output Vector and Generator Matrix Sizes

--- FilesEncryptor/helpers/HammingEncoder.cs
+++ FilesEncryptor/helpers/HammingEncoder.cs
@@ -19,29 +19,49 @@ namespace FilesEncryptor.helpers
 
                 //Determino el tamaño de los bloques de salida
                 uint outWordSize = 0;
-                double exp = 1;
+                double cantControlBits = 1;
 
-                while (Math.Pow(2, exp) < inWordSize + 1)
+                while (Math.Pow(2, cantControlBits) < cantControlBits + inWordSize + 1)
                 {
-                    exp++;
+                    cantControlBits++;
                 }
 
-                outWordSize = inWordSize + (uint)exp;
+                outWordSize = inWordSize + (uint)cantControlBits;
+
+                //Creo la matriz Generadora, reducida, sin los bits de informacion
+                List<List<EncodedString>> genMatrix = new List<List<EncodedString>>();
+
+                //La cantidad de columnas será la cantidad de bits de control
+                for (int i = 0; i < cantControlBits; i++)
+                {
+                    //La cantidad de filas esta dada por la cantidad de bits de informacion (dataBlocks.Count)
+                    genMatrix.Add(new List<EncodedString>(dataBlocks.Count));
+                }
 
                 //Creo la lista con los bloques de salida
-                List<EncodedString> outputBlocks = new List<EncodedString>(dataBlocks.Count);
-                
-                foreach(EncodedString data in dataBlocks)
-                {
-                    EncodedString encoded = data.Copy();
+                List<EncodedString> outputBlocks = new List<EncodedString>((int)outWordSize);
 
-                    for(int i=0; i < exp; i++)
+                int currentExp = 0;
+                int currentDataBit = 0;
+
+                //Inserto en los bloques de salida los bits de informacion, tal y como estan
+                for(int i = 0; i < outWordSize; i++)
+                {
+                    //Si es un bit de control, lo salteo
+                    if(i + 1 == Math.Pow(2,currentExp))
                     {
-                        encoded.Insert((uint)Math.Pow(2, i) - 1, EncodedString.ONE);
+                        currentExp++;
                     }
-                    
-                    //EncodedString encoded = new EncodedString()
-                }
+                    //Si es un bit de informacion, lo relleno con el siguiente bit de informacion de la palabra
+                    else
+                    {                        
+                        outputBlocks[i] = dataBlocks[currentDataBit];
+                        currentDataBit++;
+                    }                    
+                }                
+
+                //Ahora, calculo los bits de control de la salida
+                //TODO                 
             }
         }
     }
