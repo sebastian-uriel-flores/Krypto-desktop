86a4dd3e8b7536bb04493eab9b6d6593203f4bb0
fseba <f.seba@hotmail.com>
2017-06-10 09:28:15 -0300

    Revert "Revert "Fix hamming add extra byte""

--- FilesEncryptor/dto/FileHeader.cs
+++ FilesEncryptor/dto/FileHeader.cs
@@ -11,5 +11,10 @@ namespace FilesEncryptor.dto
         public string FileExtension { get; set; }
         public string FileName { get; set; }
         public string FileDisplayType { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("File Name: {0} - File Extension: {1} - File Display Type: {2}", FileName, FileExtension, FileDisplayType);                
+        }
     }
 }
